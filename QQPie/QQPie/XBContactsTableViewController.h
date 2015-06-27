33b03d6fa647718e7e856688a33933225fd68a6f
bos <bos@bosdeiMac.local>
2015-06-27 14:32:56 +0800

    增加子控制器

---
+++ QQPie/QQPie/XBContactsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  XBContactsTableViewController.h
+//  QQPie
+//
+//  Created by bos on 15-6-27.
+//  Copyright (c) 2015年 axiba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XBContactsTableViewController : UITableViewController
+
+@end
